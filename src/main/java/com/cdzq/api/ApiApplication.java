cbcca64adca410166910e3c53122b0e2367a2fee
52190613@qq.com <52190613@qq.com>
2020-08-11 15:20:54 +0800

    1

--- src/main/java/com/cdzq/api/ApiApplication.java
+++ src/main/java/com/cdzq/api/ApiApplication.java
@@ -2,6 +2,8 @@ package com.cdzq.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -10,7 +12,12 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableCaching
 @MapperScan(basePackages = "com.cdzq.api.mapper")
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
