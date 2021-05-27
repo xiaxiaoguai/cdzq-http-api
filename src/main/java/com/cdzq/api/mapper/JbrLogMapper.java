f274b218e55aae60cb83a53ad8a6748caf399982
52190613@qq.com <52190613@qq.com>
2021-05-28 07:38:34 +0800

    1

--- src/main/java/com/cdzq/api/mapper/JbrLogMapper.java
+++ src/main/java/com/cdzq/api/mapper/JbrLogMapper.java
@@ -2,7 +2,6 @@ package com.cdzq.api.mapper;
 
 import com.cdzq.api.base.MyMapper;
 import com.cdzq.api.entity.JbrLog;
-import org.apache.ibatis.annotations.Select;
 
 public interface JbrLogMapper extends MyMapper<JbrLog> {
 }
--- src/main/java/com/cdzq/api/mapper/WhiteipMapper.java
+++ src/main/java/com/cdzq/api/mapper/WhiteipMapper.java
@@ -2,7 +2,6 @@ package com.cdzq.api.mapper;
 
 import com.cdzq.api.base.MyMapper;
 import com.cdzq.api.entity.Whiteip;
-import org.apache.ibatis.annotations.Select;
 
 public interface WhiteipMapper extends MyMapper<Whiteip> {
 }
